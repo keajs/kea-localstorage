d665d738e8cde81e745057d417920c9a129f6be3
pribytkin <alexander.pribytkin@megafon.ru>
2019-09-07 18:12:33 +0300

    feat(Path): add params to path in localStorage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
-let storageCache = {}
+let storageCache = {};
 
-let hasLocalStorage = false
-let storage = {}
+let hasLocalStorage = false;
+let storage = {};
 
 try {
-  storage = window.localStorage
+  storage = window.localStorage;
 
-  const x = '__storage_test__'
-  storage.setItem(x, x)
-  storage.removeItem(x)
+  const x = '__storage_test__';
+  storage.setItem(x, x);
+  storage.removeItem(x);
 
-  hasLocalStorage = true
+  hasLocalStorage = true;
 } catch (e) {
   // not available
 }
 
-export default (prefix = '') => ({
+export const configure = ({ prefix = '', separator = '.' } = {}) => ({
   name: 'localStorage',
 
   // can be used globally and locally
@@ -24,32 +24,44 @@ export default (prefix = '') => ({
 
   // reducerObjects is an object with the following structure:
   // { key: { reducer, value, type, options } }
-  mutateReducerObjects (input, output, reducerObjects) {
+  mutateReducerObjects(input, output, reducerObjects) {
     if (hasLocalStorage && input.path) {
-      Object.keys(reducerObjects).filter(key => reducerObjects[key].options && reducerObjects[key].options.persist).forEach(key => {
-        const path = `${prefix}${output.path.join('.')}.${key}`
-        const defaultValue = reducerObjects[key].value
-        const defaultReducer = reducerObjects[key].reducer
+      Object.keys(reducerObjects)
+          .filter((key) => reducerObjects[key].options && reducerObjects[key].options.persist)
+          .forEach((key) => {
+            // if persist option is string than add it in path
+            const persist = reducerObjects[key].options.persist;
 
-        const value = storage[path] ? JSON.parse(storage[path]) : defaultValue
-        storageCache[path] = value
+            const path = `${prefix && prefix + separator}${
+                typeof persist === 'string' ? persist + separator : ''
+                }${output.path.join(separator)}${separator}${key}`;
 
-        const reducer = (state = value, payload) => {
-          const result = defaultReducer(state, payload)
-          if (storageCache[path] !== result) {
-            storageCache[path] = result
-            storage[path] = JSON.stringify(result)
-          }
-          return result
-        }
+            const defaultValue = reducerObjects[key].value;
+            const defaultReducer = reducerObjects[key].reducer;
 
-        reducerObjects[key].reducer = reducer
-        reducerObjects[key].value = value
-      })
+            const value = storage[path] ? JSON.parse(storage[path]) : defaultValue;
+            storageCache[path] = value;
+
+            const reducer = (state = value, payload) => {
+              const result = defaultReducer(state, payload);
+              if (storageCache[path] !== result) {
+                storageCache[path] = result;
+                storage[path] = JSON.stringify(result);
+              }
+              return result;
+            };
+
+            reducerObjects[key].reducer = reducer;
+            reducerObjects[key].value = value;
+          });
     }
   },
 
-  clearCache () {
-    storageCache = {}
+  clearCache() {
+    storageCache = {};
   }
-})
+});
+
+const plugin = configure();
+
+export default plugin;

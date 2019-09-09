9a26df3a0b161544713010f7dd4cb836c68db201
pribytkin <alexander.pribytkin@megafon.ru>
2019-09-09 21:47:11 +0300

    refactor(Path): change code style by eslint rules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-let storageCache = {};
+let storageCache = {}
 
-let hasLocalStorage = false;
-let storage = {};
+let hasLocalStorage = false
+let storage = {}
 
 try {
-  storage = window.localStorage;
+  storage = window.localStorage
 
-  const x = '__storage_test__';
-  storage.setItem(x, x);
-  storage.removeItem(x);
+  const x = '__storage_test__'
+  storage.setItem(x, x)
+  storage.removeItem(x)
 
-  hasLocalStorage = true;
+  hasLocalStorage = true
 } catch (e) {
   // not available
 }
@@ -24,44 +24,44 @@ export const configure = ({ prefix = '', separator = '.' } = {}) => ({
 
   // reducerObjects is an object with the following structure:
   // { key: { reducer, value, type, options } }
-  mutateReducerObjects(input, output, reducerObjects) {
+  mutateReducerObjects (input, output, reducerObjects) {
     if (hasLocalStorage && input.path) {
       Object.keys(reducerObjects)
-          .filter((key) => reducerObjects[key].options && reducerObjects[key].options.persist)
-          .forEach((key) => {
-            // if persist option is string than add it in path
-            const persist = reducerObjects[key].options.persist;
+        .filter((key) => reducerObjects[key].options && reducerObjects[key].options.persist)
+        .forEach((key) => {
+          // if persist option is string than add it in path
+          const persist = reducerObjects[key].options.persist
 
-            const path = `${prefix && prefix + separator}${
-                typeof persist === 'string' ? persist + separator : ''
-                }${output.path.join(separator)}${separator}${key}`;
+          const path = `${prefix && prefix + separator}${
+            typeof persist === 'string' ? persist + separator : ''
+          }${output.path.join(separator)}${separator}${key}`
 
-            const defaultValue = reducerObjects[key].value;
-            const defaultReducer = reducerObjects[key].reducer;
+          const defaultValue = reducerObjects[key].value
+          const defaultReducer = reducerObjects[key].reducer
 
-            const value = storage[path] ? JSON.parse(storage[path]) : defaultValue;
-            storageCache[path] = value;
+          const value = storage[path] ? JSON.parse(storage[path]) : defaultValue
+          storageCache[path] = value
 
-            const reducer = (state = value, payload) => {
-              const result = defaultReducer(state, payload);
-              if (storageCache[path] !== result) {
-                storageCache[path] = result;
-                storage[path] = JSON.stringify(result);
-              }
-              return result;
-            };
+          const reducer = (state = value, payload) => {
+            const result = defaultReducer(state, payload)
+            if (storageCache[path] !== result) {
+              storageCache[path] = result
+              storage[path] = JSON.stringify(result)
+            }
+            return result
+          }
 
-            reducerObjects[key].reducer = reducer;
-            reducerObjects[key].value = value;
-          });
+          reducerObjects[key].reducer = reducer
+          reducerObjects[key].value = value
+        })
     }
   },
 
-  clearCache() {
-    storageCache = {};
+  clearCache () {
+    storageCache = {}
   }
 });
 
-const plugin = configure();
+const plugin = configure()
 
-export default plugin;
+export default plugin
